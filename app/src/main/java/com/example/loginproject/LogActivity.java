be5e050e6eaf85dd263f31f50e6c345610a81b38
Felix-Silas <ygeuntae1208@naver.com>
2021-06-03 00:29:04 +0900

    Second Login

---
+++ app/src/main/java/com/example/loginproject/LogActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginproject;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class LogActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView UsernameInput = (TextView)findViewById(R.id.UsernameInput);
+        TextView PasswordInput = (TextView)findViewById(R.id.PasswordInput);
+        Intent intent = getIntent();
+        UsernameInput.setText(intent.getStringExtra("UsernameInput".toString()));
+        PasswordInput.setText(intent.getStringExtra("PasswordInput".toString()));
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/loginproject/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.loginproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText UsernameInput = (EditText) findViewById(R.id.UsernameInput);
+        final EditText PasswordInput = (EditText) findViewById(R.id.PasswordInput);
+        Button Login = (Button) findViewById(R.id.Login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = UsernameInput.getText().toString();
+            String password = PasswordInput.getText().toString();
+
+            Intent intent= new Intent(getApplicationContext(),LogActivity.class);
+            intent.putExtra("UsernameInput",name);
+            intent.putExtra("PasswordInput",password);
+            startActivity(intent);
+
+
+           }
+        });
+
+    }
+}
