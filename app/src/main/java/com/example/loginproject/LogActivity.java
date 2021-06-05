e0fcb295e3531bfd789e2b1248baf0f0f0de594c
Felix-Silas <ygeuntae1208@naver.com>
2021-06-05 16:28:33 +0900

    Fourth Login

--- app/src/main/java/com/example/loginproject/LogActivity.java
+++ app/src/main/java/com/example/loginproject/LogActivity.java
@@ -3,7 +3,6 @@ package com.example.loginproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
--- app/src/main/java/com/example/loginproject/MainActivity.java
+++ app/src/main/java/com/example/loginproject/MainActivity.java
@@ -6,8 +6,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
-import java.util.logging.Logger;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,22 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    public void onClick_login(View v){
 
-        EditText text_id = (EditText)findViewById(R.id.editText_id);
-        EditText text_pw = (EditText)findViewById(R.id.editText_pw);
-        Button onClick_login = (Button)findViewById(R.id.onClick_login);
 
+    public void onClick_login(View v) {
+
+        EditText text_id = (EditText) findViewById(R.id.editText_id);
+        EditText text_pw = (EditText) findViewById(R.id.editText_pw);
+        Button onClick_login = (Button) findViewById(R.id.onClick_login);
+        String trfa="true";
         String id = text_id.getText().toString();
         String pw = text_pw.getText().toString();
 
-        Intent intent_01= new Intent(getApplicationContext(), LogActivity.class);
+        if (pw.length() > 7 && pw.length() < 16 && id.length()>0) {
 
-        intent_01.putExtra("입력한 아이디",id);
-        intent_01.putExtra("입력한 비밀번호",pw);
+            Intent intent_01 = new Intent(getApplicationContext(), LogActivity.class);
 
-        startActivity(intent_01);
+            intent_01.putExtra("입력한 아이디", id);
+            intent_01.putExtra("입력한 비밀번호", pw);
 
+            startActivity(intent_01);
+
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"똑바로 로그인하자...",Toast.LENGTH_LONG).show();
+        }
+        }
     }
-}
