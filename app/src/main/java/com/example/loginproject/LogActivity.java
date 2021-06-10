c6ac5e10d9b1dc41e6772d7915dc0ef9e1e3a6e6
Felix-Silas <ygeuntae1208@naver.com>
2021-06-10 18:32:03 +0900

    feedback Login

--- app/src/main/java/com/example/loginproject/LogActivity.java
+++ app/src/main/java/com/example/loginproject/LogActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Button;
 import android.view.View;
 
 public class LogActivity extends AppCompatActivity {
@@ -14,9 +15,10 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Button Button_back = (Button)findViewById(R.id.Button_back);
         TextView textView_id = (TextView)findViewById(R.id.textView_id);
         TextView textView_pw = (TextView)findViewById(R.id.textView_pw);
+
         Intent intent_01 = getIntent();
 
         String id = intent_01.getStringExtra("입력한 아이디");
@@ -25,9 +27,13 @@ public class LogActivity extends AppCompatActivity {
         textView_id.setText(String.valueOf(id));
         textView_pw.setText(String.valueOf(pw));
 
+        Button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
-    public void onClick_back(View v) {
-        finish();
-    }
+
 
 }
--- app/src/main/java/com/example/loginproject/MainActivity.java
+++ app/src/main/java/com/example/loginproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,30 +18,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-
-    public void onClick_login(View v) {
-
         EditText text_id = (EditText) findViewById(R.id.editText_id);
         EditText text_pw = (EditText) findViewById(R.id.editText_pw);
-        Button onClick_login = (Button) findViewById(R.id.onClick_login);
-        String trfa="true";
-        String id = text_id.getText().toString();
-        String pw = text_pw.getText().toString();
+        Button Button_login = (Button) findViewById(R.id.Button_login);
+        final TextView Button_show = (TextView) findViewById(R.id.Button_show);
+        final int[] num = {0};
+        final int TYPE_TEXT_VARIATION_PASSWORD= 0x00000091;
+        final int TYPE_TEXT_VARIATION_VISIBLE_PASSWORD = 0x00000081;
 
-        if (pw.length() > 7 && pw.length() < 16 && id.length()>0) {
+        Button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                {
 
-            Intent intent_01 = new Intent(getApplicationContext(), LogActivity.class);
+                    String id = text_id.getText().toString();
+                    String pw = text_pw.getText().toString();
 
-            intent_01.putExtra("입력한 아이디", id);
-            intent_01.putExtra("입력한 비밀번호", pw);
+                    if (pw.length() > 7 && pw.length() < 16 && id.length() > 0) {
+                        Intent intent_01 = new Intent(getApplicationContext(), LogActivity.class);
+                        intent_01.putExtra("입력한 아이디", id);
+                        intent_01.putExtra("입력한 비밀번호", pw);
 
-            startActivity(intent_01);
+                        startActivity(intent_01);
 
-        }
-        else {
-            Toast.makeText(getApplicationContext(),"똑바로 로그인하자...",Toast.LENGTH_LONG).show();
-        }
-        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "똑바로 로그인하자...", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        Button_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int i = num[0]++;
+                if(i%2==0){
+                    Button_show.setText("Hide");
+                    text_pw.setInputType(TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                else{
+                    Button_show.setText("Show");
+                    text_pw.setInputType(TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+
+            }
+        });
     }
+
+}
+
+
+
